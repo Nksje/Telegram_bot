3bd07bec879df51e6645882d7c3088346ecea930
nksje <daniil.bondar49@gmail.com>
2023-05-28 18:24:16 +0300

    Second lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import webbrowser
 from key import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
@@ -9,10 +10,24 @@ def main(message):
     bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name}")
 
 
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://youtube.com')
+
+
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(
         message.chat.id, "<b>Help information</b>", parse_mode='html')
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == "hello":
+        bot.send_message(
+            message.chat.id, f"Hello, {message.from_user.first_name} {message.from_user.last_name}")
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f"ID: {message.from_user.id}")
 
 
 bot.polling(none_stop=True)
